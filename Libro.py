14dad7a3805d4c1bb4ff471d9c732f5f4c1a8db1
xDennis20 <dennis_6@outlook.com>
2026-01-19 16:09:06 -0500

    Add: Clase Libro

---
+++ Libro.py
@@ -0,0 +1,59 @@
+class Libro:
+    TIPO_DIGITAL = "digital"
+    TIPO_FISICO = "fisico"
+    TIPOS_VALIDOS = (TIPO_DIGITAL,TIPO_FISICO)
+
+    def __init__(self, titulo: str, autor: str, isbn: str, years: str):
+        if self.validar_str_vacios(titulo) or self.validar_str_vacios(autor) or self.validar_str_vacios(isbn) or self.validar_str_vacios(years):
+            raise ValueError("Error: Valor vacio")
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.years = years
+        self.tipo = Libro.TIPO_DIGITAL
+        self.stock = None
+
+    @staticmethod
+    def validar_str_vacios(valor: str) -> bool:
+        valor = valor.strip(" ")
+        if not valor:
+            return True
+        return False
+
+    @staticmethod
+    def validar_tipo_libro(tipo: str):
+        return tipo.lower() in Libro.TIPOS_VALIDOS
+
+    @classmethod
+    def libro_digital(cls,titulo: str, autor: str, isbn: str, years:str) -> Libro:
+        libro = cls(titulo,autor,isbn,years)
+        libro.tipo = cls.TIPO_DIGITAL
+        return libro
+
+    @classmethod
+    def libro_fisico(cls,titulo:str,autor: str, isbn: str, years: str, stock: int) -> Libro:
+        libro = cls(titulo,autor,isbn,years)
+        libro.tipo = cls.TIPO_FISICO
+        libro.stock = stock
+        return libro
+
+    def __eq__(self, other):
+        if not isinstance(other,Libro):
+            raise NotImplemented
+        return self.isbn == other.isbn
+
+    def __lt__(self, other):
+        if not isinstance(other, Libro):
+            raise NotImplemented
+        return self.titulo < other.titulo
+
+    def __str__(self) -> str:
+        if self.tipo == Libro.TIPO_DIGITAL:
+            return f"Libro: {self.titulo}, Autor: {self.autor} ISBN: {self.isbn}, Año: {self.years} Tipo: {self.tipo}"
+        return f"Libro: {self.titulo}, Autor: {self.autor} ISBN: {self.isbn}, Año: {self.years} Tipo: {self.tipo}, Stock: {self.stock}"
+
+    def __repr__(self):
+        return f"{type(self).__name__}(titulo='{self.titulo}', autor='{self.autor}, isbn='{self.isbn}, año='{self.years}', tipo='{self.tipo}', stock={self.stock}"
+
+l1 = Libro.libro_fisico("pinocho","nose","12312314124213","2014", stock= 5)
+l2 = Libro.libro_digital("nose","21312312", "s123123213", "2304")
