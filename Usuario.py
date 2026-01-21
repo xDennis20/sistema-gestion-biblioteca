e779d15fba0c96a2fbb757eb9c6b15cc8301b25b
xDennis20 <dennis_6@outlook.com>
2026-01-21 11:27:15 -0500

    add: Agregue 2 metodos y un atributo mas Clase Usuario

--- Usuario.py
+++ Usuario.py
@@ -14,6 +14,7 @@ class Usuario:
         self.correo = correo
         self.tipo = Usuario.TIPO_REGULAR
         self.libros_prestados : list[Libro] = []
+        self.multa = None
 
     @classmethod
     def usuario_regular(cls,nombre: str, dni: str, correo: str) -> Usuario:
@@ -27,6 +28,23 @@ class Usuario:
         usuario.tipo = cls.TIPO_VIP
         return usuario
 
+    def agregar_libro_usuario(self,libro: Libro):
+        self.libros_prestados.append(libro)
+
+    def devolver_libro(self,libro: Libro):
+        if libro in self.libros_prestados:
+            self.libros_prestados.remove(libro)
+        else:
+            print(f"El usuario {self.nombre} no tiene el libro {libro.titulo}")
+
     @property
     def puede_pedir_mas(self):
         return len(self.libros_prestados) < Usuario.TIPO_LIMITES.get(self.tipo)
+
+    def __eq__(self, other):
+        if not isinstance(other,Usuario):
+            return NotImplemented
+        return self.dni == other.dni
+
+    def __str__(self):
+        return f"Usuario {self.nombre}, DNI: {self.dni}, Correo: {self.correo}, Tipo: {self.tipo}"
