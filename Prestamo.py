ff19a990aae63ad53240f08b625020ceef3d51de
xDennis20 <dennis_6@outlook.com>
2026-01-21 11:29:25 -0500

    add: Agregue 2 getters mas y una atributo de clase a la clase Prestamo

--- Prestamo.py
+++ Prestamo.py
@@ -4,6 +4,7 @@ from datetime import date, datetime, timedelta
 
 class Prestamo:
     PLAZO_PRESTADO = 14
+    COSTO_MULTA_DIARA = 0.50
 
     def __init__(self,usuario: Usuario,fecha_prestamo: date,libro: Libro):
         self.usuario = usuario
@@ -21,7 +22,23 @@ class Prestamo:
         return cls(usuario,datetime.now().date(),libro)
 
     @property
-    def retraso_dias(self):
+    def retraso_dias(self) -> int:
         fecha_actual = datetime.now().date()
         dias_retraso = fecha_actual - self.fecha_vencimiento
         return max(0,dias_retraso.days)
+
+    @property
+    def multa(self) -> float:
+        if self.usuario.tipo == Usuario.TIPO_VIP:
+            return 0.0
+
+        if self.retraso_dias <= 0:
+            return 0.0
+
+        return Prestamo.COSTO_MULTA_DIARA * self.retraso_dias
+
+    def __str__(self):
+        return (f"--- Pretamo ---"
+                f"Usuario: {self.usuario.nombre}"
+                f"Libro: {self.libro.titulo}"
+                f"Fecha_vencimiento: {self.fecha_vencimiento}")
